ca960f86bfb06721e25742a8fb59a41ce292e784
Paweł Radej <pawel.radej@outlook.com>
2025-03-16 08:38:08 +0100

    Added 1024x768 default resolution

--- src/resolutions.ts
+++ src/resolutions.ts
@@ -5,5 +5,6 @@ export const resolutions: Resolution[] = [
   { width: 720, height: 480 },
   { width: 720, height: 576 },
   { width: 720, height: 720 },
+  { width: 1024, height: 768 },
   { width: 1280, height: 720 },
 ];
