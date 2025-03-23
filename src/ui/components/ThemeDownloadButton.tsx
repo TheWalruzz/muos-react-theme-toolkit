e1ea96ddcd601716f4b5acbf79c7f933a7743c66
Paweł Radej <pawel.radej@outlook.com>
2025-03-23 10:22:35 +0100

    Added processing progress percentage

--- src/ui/components/ThemeDownloadButton.tsx
+++ src/ui/components/ThemeDownloadButton.tsx
@@ -1,10 +1,17 @@
+import { useMemo } from "react";
 import { themes } from "@/config";
 import { useDownloadTheme } from "../hooks/useDownloadTheme";
 
 import styles from "./ThemeDownloadButton.module.css";
 
 export function ThemeDownloadButton() {
-  const { isProcessing, downloadTheme } = useDownloadTheme();
+  const { isProcessing, downloadTheme, progress, totalProgress } =
+    useDownloadTheme();
+
+  const progressDisplay = useMemo(
+    () => Math.floor((progress / totalProgress) * 100),
+    [progress, totalProgress]
+  );
 
   return (
     <div className={styles.ThemeDownloadButton}>
@@ -17,7 +24,7 @@ export function ThemeDownloadButton() {
         onClick={downloadTheme}
         disabled={themes.length === 0 || isProcessing}
       >
-        Download
+        {isProcessing ? `Processing... (${progressDisplay}%)` : "Download"}
       </button>
     </div>
   );
