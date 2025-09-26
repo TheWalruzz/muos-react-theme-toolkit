ff88640ebe522b9fdb79eae0a89cb2be96537ef0
Paweł Radej <pawel.radej@outlook.com>
2025-09-26 19:59:05 +0200

    Added proper Korean language support

--- src/locales/index.ts
+++ src/locales/index.ts
@@ -2,8 +2,10 @@ import { SupportedTranslations } from "./supportedLanguages";
 
 import en from "./en.json";
 import pl from "./pl.json";
+import ko from "./ko.json";
 
 export const resources: SupportedTranslations = {
   en: { translation: en },
   pl: { translation: pl },
+  ko: { translation: ko },
 };
--- src/themes/Fluent/index.tsx
+++ src/themes/Fluent/index.tsx
@@ -196,7 +196,7 @@ export const fluentLight: ThemeConfig = {
       "round(calc(var(--height) / var(--header-height-divider)), 1px)",
     "--padding": "8px",
   },
-  languages: ["en", "pl"],
+  languages: ["en", "pl", "ko"],
   fallbackLanguage: "en",
 };
 
