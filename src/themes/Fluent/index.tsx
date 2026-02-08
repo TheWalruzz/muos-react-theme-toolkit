f31ac0d8d5c1f87e8c4758e05cee9d0775c0a992
Paweł Radej <pawel.radej@outlook.com>
2026-02-08 09:28:22 +0100

    Added more new glyphs

--- src/themes/Fluent/index.tsx
+++ src/themes/Fluent/index.tsx
@@ -1,5 +1,5 @@
 import "./index.css";
-import { ThemeConfig, Resolution } from "@/types";
+import { ThemeConfig, Resolution, ScreenConfig } from "@/types";
 import { assets } from "./assets";
 import { defaultScheme } from "./schemes/default";
 import { muxlaunchScheme } from "./schemes/muxlaunch";
@@ -38,6 +38,54 @@ import { OverlayIndicator } from "./components/OverlayIndicator";
 //   query: "?url",
 //   import: "default",
 // });
+
+const getOverlayScreens = (): ScreenConfig[] => [
+  ...Array.from(Array(10).keys()).map((index) => ({
+    path: `battery_${index}.png`,
+    ignoreInLocalized: true,
+    pathPrefix: "overlay/battery/",
+    overrideResolution: ({ width, height }: Resolution) => ({
+      width: Math.round(width / 3.5),
+      height: Math.round(height / 10),
+    }),
+    render: ({ height }: Resolution) => (
+      <OverlayIndicator
+        index={index}
+        icon={<BatteryChargeRegular fontSize={Math.round(height / 18)} />}
+      />
+    ),
+  })),
+  ...Array.from(Array(10).keys()).map((index) => ({
+    path: `bright_${index}.png`,
+    ignoreInLocalized: true,
+    pathPrefix: "overlay/bright/",
+    overrideResolution: ({ width, height }: Resolution) => ({
+      width: Math.round(width / 3.5),
+      height: Math.round(height / 10),
+    }),
+    render: ({ height }: Resolution) => (
+      <OverlayIndicator
+        index={index}
+        icon={<BrightnessHighRegular fontSize={Math.round(height / 18)} />}
+      />
+    ),
+  })),
+  ...Array.from(Array(10).keys()).map((index) => ({
+    path: `volume_${index}.png`,
+    ignoreInLocalized: true,
+    pathPrefix: "overlay/volume/",
+    overrideResolution: ({ width, height }: Resolution) => ({
+      width: Math.round(width / 3.5),
+      height: Math.round(height / 10),
+    }),
+    render: ({ height }: Resolution) => (
+      <OverlayIndicator
+        index={index}
+        icon={<Speaker2Regular fontSize={Math.round(height / 18)} />}
+      />
+    ),
+  })),
+];
 
 const fluentBase: ThemeConfig = {
   name: "Fluent Base",
@@ -111,99 +159,11 @@ const fluentBase: ThemeConfig = {
       render: () => <MainMenu itemIndex={7} showBackground={false} />,
     },
     // overlays for brightness, volume and battery
-    ...Array.from(Array(10).keys()).map((index) => ({
-      path: `battery_${index}.png`,
-      ignoreInLocalized: true,
-      pathPrefix: "overlay/battery/",
-      overrideResolution: ({ width, height }: Resolution) => ({
-        width: Math.round(width / 3.5),
-        height: Math.round(height / 10),
-      }),
-      render: ({ height }: Resolution) => (
-        <OverlayIndicator
-          index={index}
-          icon={<BatteryChargeRegular fontSize={Math.round(height / 18)} />}
-        />
-      ),
-    })),
-    ...Array.from(Array(10).keys()).map((index) => ({
-      path: `bright_${index}.png`,
-      ignoreInLocalized: true,
-      pathPrefix: "overlay/bright/",
-      overrideResolution: ({ width, height }: Resolution) => ({
-        width: Math.round(width / 3.5),
-        height: Math.round(height / 10),
-      }),
-      render: ({ height }: Resolution) => (
-        <OverlayIndicator
-          index={index}
-          icon={<BrightnessHighRegular fontSize={Math.round(height / 18)} />}
-        />
-      ),
-    })),
-    ...Array.from(Array(10).keys()).map((index) => ({
-      path: `volume_${index}.png`,
-      ignoreInLocalized: true,
-      pathPrefix: "overlay/volume/",
-      overrideResolution: ({ width, height }: Resolution) => ({
-        width: Math.round(width / 3.5),
-        height: Math.round(height / 10),
-      }),
-      render: ({ height }: Resolution) => (
-        <OverlayIndicator
-          index={index}
-          icon={<Speaker2Regular fontSize={Math.round(height / 18)} />}
-        />
-      ),
-    })),
+    ...getOverlayScreens(),
     // duplicate the 640x480 as the default overlays
-    ...Array.from(Array(10).keys()).map((index) => ({
-      path: `battery_${index}.png`,
-      ignoreInLocalized: true,
+    ...getOverlayScreens().map((screen) => ({
+      ...screen,
       ignoreOtherResolutions: true,
-      pathPrefix: "overlay/battery/",
-      overrideResolution: ({ width, height }: Resolution) => ({
-        width: Math.round(width / 3.5),
-        height: Math.round(height / 10),
-      }),
-      render: ({ height }: Resolution) => (
-        <OverlayIndicator
-          index={index}
-          icon={<BatteryChargeRegular fontSize={Math.round(height / 18)} />}
-        />
-      ),
-    })),
-    ...Array.from(Array(10).keys()).map((index) => ({
-      path: `bright_${index}.png`,
-      ignoreInLocalized: true,
-      ignoreOtherResolutions: true,
-      pathPrefix: "overlay/bright/",
-      overrideResolution: ({ width, height }: Resolution) => ({
-        width: Math.round(width / 3.5),
-        height: Math.round(height / 10),
-      }),
-      render: ({ height }: Resolution) => (
-        <OverlayIndicator
-          index={index}
-          icon={<BrightnessHighRegular fontSize={Math.round(height / 18)} />}
-        />
-      ),
-    })),
-    ...Array.from(Array(10).keys()).map((index) => ({
-      path: `volume_${index}.png`,
-      ignoreInLocalized: true,
-      ignoreOtherResolutions: true,
-      pathPrefix: "overlay/volume/",
-      overrideResolution: ({ width, height }: Resolution) => ({
-        width: Math.round(width / 3.5),
-        height: Math.round(height / 10),
-      }),
-      render: ({ height }: Resolution) => (
-        <OverlayIndicator
-          index={index}
-          icon={<Speaker2Regular fontSize={Math.round(height / 18)} />}
-        />
-      ),
     })),
     // uncomment only if you want to regenerate grid images
     // ...Object.keys(systemIcons).map((system) => ({
