5b99ffe738ea86c529e2624426d31c96887cf154
Paweł Radej <pawel.radej@outlook.com>
2026-05-31 15:51:21 +0200

    Replaced most glyphs with svg icons

--- src/themes/Fluent/assets/glyph/index.ts
+++ src/themes/Fluent/assets/glyph/index.ts
@@ -1,9 +1,18 @@
-import { importBinaryAssets } from "@/utils/importAssets";
+import { importBinaryAssets, importTextAssets } from "@/utils/importAssets";
 
-export const glyphAssets = importBinaryAssets(
-  import.meta.glob("./**/*.{png,bmp,gif}", {
-    eager: true,
-    query: "?data-url",
-    import: "default",
-  })
-);
+export const glyphAssets = [
+  ...importBinaryAssets(
+    import.meta.glob("./**/*.{png,bmp,gif}", {
+      eager: true,
+      query: "?data-url",
+      import: "default",
+    }),
+  ),
+  ...importTextAssets(
+    import.meta.glob("./**/*.svg", {
+      eager: true,
+      query: "?raw",
+      import: "default",
+    }),
+  ),
+];
