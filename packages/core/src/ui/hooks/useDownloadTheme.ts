b921e609f4abd3ca023ba3d1a7f2bae74336ef1f
Paweł Radej <pawel.radej@outlook.com>
2026-07-09 17:32:51 +0200

    Lowered compression of zip files, since images are already compressed

--- packages/core/src/ui/hooks/useDownloadTheme.ts
+++ packages/core/src/ui/hooks/useDownloadTheme.ts
@@ -19,7 +19,7 @@ import imageCompression from "browser-image-compression";
 
 const defaultZipOptions = {
   bufferedWrite: true,
-  level: 4,
+  level: 2,
 };
 
 const optimizePNG = async (dataUrl: string, filename: string) => {
